c1bfd9c80771d31d8ab8e6151bd42d87a68ccb01
0-tae <100774819+0-tae@users.noreply.github.com>
2022-11-06 04:56:33 +0900

    초기 파일 업로드

---
+++ Login_Page.js
@@ -0,0 +1,6 @@
+const button_Login = document.getElementById("button_Login");
+const button_Resistration = document.getElementById("button_Resistration");
+
+function process_Resistration() {}
+
+button_Resistration.addEventListener("click", process_Resistration);
